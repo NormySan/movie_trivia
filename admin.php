29bd288cfa8b4954e97b68464426813718163ad6
sebastian lockwood <sebastian.lockwood@gmail.com>
2013-11-12 23:09:35 +0100

    Created Admin Template and cleaned up admin.php
    Template Looks horrific without bootstrap included =)

--- admin.php
+++ admin.php
@@ -1,3 +1,4 @@
 <?php
 
-// Admin.php
+// Are we really going to use this file?
+// When www.thequiz.com/admin redirects to the template file /templates/admin.php ? 
--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,101 @@
-<h1>Hello, Admin!</h1>
+<!DOCTYPE html>
+
+    <!--|  LOOKS REALLY BAD WITHOUT BOOTSTRAP INCLUDED =)  |-->
+
+<div class="container">           
+    <div class="page-header">
+        <img src="img/admin_logo.jpg" class="img-responsive" alt="admin logo">
+        <h1>Admin<h1>
+        <small>
+            Manage your Quizdata here
+        </small>
+        </h1>
+    </div>
+
+
+    <div class="row">                          
+        <div class="col-md-6">
+            <h3>Add a Question</h3>
+            <form role="form" method="post">
+                
+                <div class="form-group">
+                    <label for="question">Question:</label>
+                    <textarea class="form-control" rows="4" cols="50" id="question" placeholder="What is the meaning of life.." required></textarea>
+                </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="answer1">Answer 1:</label>
+                            <input type="text" class="form-control" id="answer1" placeholder="The Question.." required>
+                        </div>                   
+                        <div class="form-group">
+                            <label for="answer2">Answer 2:</label>
+                            <input type="text" class="form-control" id="answer2" placeholder="The Answers." required>
+                        </div> 
+                    </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="answer3">Answer 3:</label>
+                        <input type="text" class="form-control" id="answer3" placeholder="The Answers.." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="answer4">Answer 4:</label>
+                        <input type="text" class="form-control" id="answer4" placeholder="The Answers.." required>
+                    </div>
+                </div>
+                <div class="col-md-6>">
+                    <div class="form-group">
+                        <label for="addToCategory">Add to Category:</label>
+                        <select id="addToCategory" class="form-control">
+                            <option value="0"><?php if(!isset($categories))print('No Categories Found..'); else print('Choose Category');?> </option>
+                            <?php foreach($categories as $menuItem): ?>
+                            <option value="<?php print($menuItem['id'])?>"><?php print($menuItem['name']);?> </option>
+                            <?php endforeach;?>
+                        </select>
+                    </div>
+                </div>                    
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+            
+            <div class="col-md-6">        
+                <h3>Add a category</h3>
+                <form method="post" name="addCategory" id="addCategory">
+                    <div class="form-group">
+                        <label for="addCategoryName">Name:</label>
+                        <input type="text" class="form-control" id="addCategoryName" placeholder="A Name.." required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>              
+            </div>  
+
+        </div>
+        
+        <div class="col-md-6">
+            <h3>Remove Stuff</h3>
+            <form method="post" action="" name="removeQuestions" id="removeQuestions">
+                <div class="form-group">
+                    <label for="deleteQuestion">Remove Question:</label>
+                    <select id="deleteQuestion" class="form-control">
+                        <option value="0"><?php if(!isset($questions))print('No Questions Found..'); else print('Choose Question');?> </option>
+                        <?php foreach($questions as $menuItem): ?>
+                        <option value="<?php print($menuItem['id'])?>"><?php print($menuItem['name']);?> </option>
+                        <?php endforeach;?>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+
+            <form method="post" name="removeCategory" id="removeCategory">
+                <div class="form-group">
+                    <label for="deleteCategory">Remove Category:</label>
+                    <select id="deleteCategory" class="form-control">
+                        <option value="0"><?php if(!isset($categories))print('No Categories Found..'); else print('Choose Category');?> </option>
+                        <?php foreach($categories as $menuItem): ?>
+                        <option value="<?php print($menuItem['id'])?>"><?php print($menuItem['name']);?> </option>
+                        <?php endforeach;?>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+    </div>   
+</div>
