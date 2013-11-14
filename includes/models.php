0976cf06805b50304a4524d73147fa73ff7eeec5
Christoffer <palm.christoffer@gmail.com>
2013-11-14 11:29:56 +0100

    Added getQuestions function and (reverted from commit d51468fe421c480835dd6e616e4a18328a3d677c)

--- includes/models.php
+++ includes/models.php
@@ -54,22 +54,64 @@ function saveCategory($menuAddName)
 }
 
 // Returns all questions with answers
-function getQuestions()
+function getQuestions($options = array())
 {
 	global $db;
 
+	// Create an empty questions array so that we always return an empty
+	// array even if there was no data fetched from the database.
 	$questions = array();
 
-	$results = $db->query('SELECT title FROM questions, answers');
+	// Empty array for andswers
+	$answers = array();
 
+	// Empty array for ids
+	$ids = array();
+
+	// The database query to get the questions.
+	$results = $db->query('SELECT * FROM questions');
+
+	// Do a while loop to fetch each resulting row in the query.
 	while ($row = $results->fetch(PDO::FETCH_ASSOC))
 	{
+		// Push each result row into the questions array.
 		$questions[] = $row;
 	}
 
+	// Loop over the questions and get their ids
+	foreach ($questions as $question)
+	{
+		// Push each question id into the ids array
+		$ids[] = $question['id'];
+	}
+
+	// Concatenate all questions ids into a string of ids to be used in the query
+	$ids = implode(',', $ids);
+
+	// The database query to fetch answers.
+	$statement = $db->query("SELECT * FROM questions_answers qa
+						   	 LEFT JOIN answers a ON a.id = qa.answer_id
+						   	 WHERE qa.question_id IN ($ids)");
+
+	// Get each answer and push it onto the answers array
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+	{
+		$answers[] = $row;
+	}
+
+	// Push each answer into the questions array
+	foreach ($questions as $index => $question)
+	{
+		foreach ($answers as $answer)
+		{
+			if ($answer['question_id'] == $question['id'])
+			{
+				$questions[$index]['answers'][] = $answer;
+			}
+		}
+	}
+
 	return $questions;
-
-
 }
 
 function getQuestion($id){
