a354a73ec275213870a7f62dd801561cbde2d990
Costner <m.ruden.91@gmail.com>
2013-11-18 14:56:10 +0100

    added tempaltes

---
+++ templates/ingame.php
@@ -0,0 +1,51 @@
+<div id="wrapper" class="centered" >
+	<div class="container">
+		<hr>
+	 	<div class="jumbotron jumbo-color1 box-shadow">
+	     	<h1 class="txt-shadow">Movie Trivia</h1>
+	     	<p class="txt-shadow">Chosen category: Horror</p>
+	     	<p class="txt-shadow">4/7</p>
+	    </div>
+	</div>
+	<div class="container">
+		<div class=""></div>
+	</div>
+
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-lg-6 centered">
+					<p class="lead"> Who is Batman and what does he do?</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-lg-6 centered">
+					<button type="button" class="btn btn-default cat-btn cat-btn1">Alternative1</button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-lg-6 centered">
+					<button type="button" class="btn btn-default cat-btn cat-btn1">Alternative2</button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-lg-6 centered">
+					<button type="button" class="btn btn-default cat-btn cat-btn1">Alternative3</button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-lg-6 centered">
+					<button type="button" class="btn btn-default cat-btn cat-btn1">Alternative4</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
---
+++ templates/startgame.php
@@ -0,0 +1,52 @@
+<div id="wrapper" class="centered" >
+
+    <div class="container">
+    	<hr>
+	 	<div class="jumbotron jumbo-color1 box-shadow">
+            <h1>Movie Trivia</h1>
+            <p>Tjena spela spel är kul! ös Play!</p>
+        </div>
+    </div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn1">Horror</button>
+			</div>
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn2">Costume Drama</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn3">Drama</button>
+			</div>
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn4">Comedy</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn5">Action</button>
+			</div>
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn6">Sci-Fi</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<button type="button" class="btn btn-default cat-btn cat-btn7">Fantasy</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12" style="padding: 20px 0 0 0;">
+				<div class="col-lg-10"></div>
+				<div class="col-lg-2">
+					<button type="submit" value="#" onclick="#" class="btn btn-primary btn-100p">START</button>
+				</div>
+			</div>
+		</div>
+			
+		
+	</div>
+</div>
