340bf28cb97cd2af4be6d22791d06017c3eaee88
Christoffer <palm.christoffer@gmail.com>
2013-11-20 10:22:30 +0100

    QuestionsController now returns 10 random questions

--- includes/controllers.php
+++ includes/controllers.php
@@ -8,31 +8,16 @@ function HomeController()
 
 function QuestionsController()
 {
-	$questions = array(
-		array(
-			'id' => 1,
-			'title' => 'Who is batman',
-			'answers' => array(
-				array('title' => 'A superhero'),
-				array('title' => 'A garden tool'),
-				array('title' => 'A rock made from paper'),
-				array('title' => 'A fisherman')
-			)
-		),
-		array(
-			'id' => 2,
-			'title' => 'Who was the director for Titanic (1997)',
-			'answers' => array(
-				array('title' => 'Joseph Kosinski', 'correct' => false),
-				array('title' => 'James cameron', 'correct' => true),
-				array('title' => 'M. Night Shyamalan', 'correct' => false),
-				array('title' => 'Martin Scorsese', 'correct' => false)
-			)
-		)
-	);
+	// Check if id is set
+	$id = isset($_GET['id']) ? $_GET['id'] : null;
 
+	// Get random questions
+	$questions = getRandQuestion($id);
+
+	// JSON encode the questions
 	$questions = json_encode($questions);
 
+	// Return the JSOn encoded string
 	response($questions);
 }
 
